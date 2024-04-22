83822b329a53b34ab6e85debd18663b51313153c
benank <terkolskie@gmail.com>
2024-04-21 20:33:41 -0400

    Remove unncessary eslint disable

--- client/src/cfx/StateBagChangeHandler.ts
+++ client/src/cfx/StateBagChangeHandler.ts
@@ -1,4 +1,3 @@
-/* eslint-disable  @typescript-eslint/no-explicit-any */
 export interface StateBagChangeHandler {
 	(bagName: string, key: string, value: any, reserved: number, replicated: boolean): void;
 }
